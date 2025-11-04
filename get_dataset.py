e4234c1fecf80bfb9eb2314f773b43f59381d7be
Thijs Brits <thijsbrits@gmail.com>
2025-11-04 14:19:49 +0100

    Refactor dataset retrieval in get_dataset.py to improve data handling and format. Update mccmnc_dataset.json with lowercase keys for consistency. Add lxml to requirements.txt for enhanced HTML parsing.

--- get_dataset.py
+++ get_dataset.py
@@ -1,5 +1,18 @@
 import json
 import pandas as pd
 
-with open("mccmnc_daataset.json", "w") as f:
-    json.dump(pd.read_html("https://www.mcc-mnc.com/")[0].to_dict(orient="records"), f)
+def get_mccmnc_dataset():
+    url = "https://mcc-mnc.com/"
+    # we need to know the number of columns to read them in all as string with converters
+    n_columns = pd.read_html(url)[0].shape[1]
+
+    mccmnc = pd.read_html(url, converters={i: str for i in range(n_columns)})[0]
+    mccmnc.columns = mccmnc.columns.map(lambda x: x.lower().replace(" ", "_"))
+
+    return mccmnc
+
+if __name__ == "__main__":
+    mccmnc = get_mccmnc_dataset()
+    print(mccmnc.head())
+    with open("mccmnc_dataset.json", "w") as f:
+        json.dump(mccmnc.to_dict(orient="records"), f)
