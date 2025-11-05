39ceba65c937d6ba09618bf3fd35e4a7e675d80e
Thijs Brits <thijsbrits@gmail.com>
2025-11-05 10:35:30 +0100

    Enhance dataset retrieval by filling Wallis and Futuna country code and adding a test for null values in the dataset.

--- get_dataset.py
+++ get_dataset.py
@@ -8,6 +8,10 @@ def get_mccmnc_dataset():
 
     mccmnc = pd.read_html(url, converters={i: str for i in range(n_columns)})[0]
     mccmnc.columns = mccmnc.columns.map(lambda x: x.lower().replace(" ", "_"))
+    
+
+    # Fill Wallis and Futuna with MCC 681
+    mccmnc.loc[mccmnc['country'] == 'Wallis and Futuna', 'country_code'] = '681'
     mccmnc.fillna("", inplace=True)
 
     return mccmnc
@@ -17,10 +21,16 @@ def test_read_json():
         mccmnc = json.load(f)
     print(mccmnc)
 
+def test_no_nulls():
+    mccmnc = get_mccmnc_dataset()
+    assert mccmnc.isnull().sum().sum() == 0
+    
+
 if __name__ == "__main__":
     mccmnc = get_mccmnc_dataset()
     print(mccmnc.head())
+    test_no_nulls()
+    
     with open("mccmnc_dataset.json", "w") as f:
         json.dump(mccmnc.to_dict(orient="records"), f)
-
     test_read_json()
